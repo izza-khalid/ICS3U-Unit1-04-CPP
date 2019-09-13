07e925b8d349c6184e2bb0752ceb029d5e2c73a3
Ubuntu <ubuntu@ip-172-31-29-95.ec2.internal>
2019-09-13 18:53:15 +0000

    initial message

---
+++ doing_math_two.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2019 Izza Khalid All rights reserved
+//
+// Created by: Izza Khalid
+// Created on: September 2019
+// This program calculates the area and perimeter of a rectangle with dimensions
+// 5cm x 3cm
+
+#include <iostream>
+
+int main() {
+    std::cout << "If the rectangle has the dimensions:" << std::endl;
+    std::cout << "5cm x 3cm" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Area is " << (3*5) << "cm^2" << std::endl;
+    std::cout << "Perimeter is " << (2*(3+5)) << "cm" << std::endl;
+}
